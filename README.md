2038a961d87696c4fa8fbc65d5a2f8991cfaa338
DominicNikolai <d960960gm@gmail.com>
2026-02-12 17:49:19 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Foto Preview](preview/project-484.avif)](https://DominicNikolai.github.io/project-484)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/DominicNikolai/project-483" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/DominicNikolai/project-485" target="_blank">&#8594;</a>
+</div>
---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assets: 'static',
+    
+  },
+  //Deploy normal project
+  // base: '/project-484/',
+  // site: 'https://dominicnikolai.github.io/project-484/'
+
+  //Deploy 20 Projects
+  // base: '/20-projects-with-css/',
+  // site: 'https://20essentials.github.io/20-projects-with-css/'
+
+  //Localhost
+  site: 'http://localhost:4321/'
+});
